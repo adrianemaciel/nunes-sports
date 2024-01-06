ad7678d1a574047378028f2cdfe612ba24019a78
adrianemaciel <adrianemaciel.developer@gmail.com>
2024-01-06 18:12:05 -0300

    row: database installation and configuration

--- server/index.js
+++ server/index.js
@@ -1,9 +1,31 @@
 const express = require("express");
 const app = express();
+const mysql = require("mysql2");
+const cors = require("cors");
+
+const connection = mysql.createConnection({
+  host: "localhost",
+  user: "root",
+  password: "Aomcl",
+  database: "nunes_sports",
+});
 
 app.get("/", (req, res) => {
-  res.send("hello word");
+  return res.json({ status: "ok" });
 });
+
+app.get("/products", (req, res) => {
+  connection.query(
+    "SELECT * FROM nunes_sports.products",
+    function (err, results) {
+      console.log(results); // results contains rows returned by server
+      return res.json(results);
+    }
+  );
+});
+
+app.use(cors());
+app.use(express.json());
 
 app.listen(3001, () => {
   console.log("rodando servidor");
